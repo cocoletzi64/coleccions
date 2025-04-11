160286b54640078d3a164cb3a631b12b512cb656
Erick Cocoletzi <erickalbertosantillancocoletzi@gmail.com>
2025-04-11 00:19:21 -0600

    linkedlist

--- src/arraylist.java
+++ src/arraylist.java
@@ -3,21 +3,19 @@ import java.util.*;
 public class arraylist {
     public static void main(String[] args) {
 
-        List<Persona> lista = new ArrayList<Persona>();
+        List<Persona> lista = new LinkedList<Persona>();
+
         lista.add(new Persona("Erick",25));
         lista.add(new Persona("Luisina",30));
         lista.add(new Persona("Gabriela",15));
         lista.add(new Persona("Luis",12));
+        //agregar al principio
+        lista.add(0,new Persona("Dolores",53));
+        lista.add(5,new Persona("Dolores",53));
 
-        //reccorer
-        System.out.println("----------- FOR-------------");
-        for(int i=0; i<lista.size(); i++){
-            System.out.println("ID: "+lista.get(i).getNumid());
-            System.out.println("NOMBRE: "+lista.get(i).getNombre());
-            System.out.println("EDAD: "+lista.get(i).getEdad());
-        }
+
+
         System.out.println("-----------FOREACH-------------");
-
         //foreach
         for(Persona perso:lista){
             System.out.println("ID: "+perso.getNumid());
